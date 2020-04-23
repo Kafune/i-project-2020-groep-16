4de26600b4efac1dd5c73bd9d82951eefb8c52c2
Coolcatss <j.vanaalst58@gmail.com>
2020-04-23 12:36:58 +0200

    Index omgeschreven naar html met php include

--- index.php
+++ index.php
@@ -4,3 +4,12 @@ include_once ("includes/footer.php");
 ?>
 <link rel="stylesheet" href="styles/css/mystyles.css">
 <title>Home</title>
+
+<body>
+<div class="hero is is-fullheight">
+    <figure class="image">
+        <img src="sources/background%201.gif" height="400px">
+
+    </figure>
+</div>
+</body>
