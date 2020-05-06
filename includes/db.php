fba09e89209f994af8c3009936e98b48a79bfe76
Coolcatss <j.vanaalst58@gmail.com>
2020-05-06 11:20:56 +0200

    db connectie

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,11 @@
 <?php
-$gebruiker = "root";
-$wachtwoord = "";
+$servername = "mssql2.iproject.icasites.nl";
+$gebruiker = "iproject16";
+$wachtwoord = "zv1VeSWK";
+$db = "iproject16";
 
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=eenmaalandermaal', $gebruiker, $wachtwoord);
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $gebruiker, $wachtwoord, $db);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Er is iets fout<br>{$e->getMessage()}";
--- includes/root.php
+++ includes/root.php
@@ -1,9 +1,9 @@
 <?php
 // root pad. Nodig voor alle include bestanden
-define('ROOT', $_SERVER['DOCUMENT_ROOT'].'/KL/i-project');
+define('ROOT', $_SERVER['DOCUMENT_ROOT'].'');
 
 // root pad. Nodig voor html href links.
-$root = '/KL/i-project';
+$root = '';
 
 // Dit is relevant voor alle pagina's die iets in de pagina hebben wat afwijkt van andere pagina's.
 // Bijvoorbeeld contact pagina met google maps of admin pagina's zonder header
