d036ddcae4f6f3f08293dc7417fd6ee49a5c7ed3
Coolcatss <j.vanaalst58@gmail.com>
2020-05-07 10:28:04 +0200

    uitlog script

--- scripts/logout.php
+++ scripts/logout.php
@@ -3,16 +3,16 @@
 session_start();
 
 // als variabel gebruikersnaam bestaat dan toegang tot de code eronder
-if(isset($_SESSION["gebruikersnaam"])) {
+if (isset($_SESSION["gebruikersnaam"])) {
 // zet alle variabelen naar null array
     $_SESSION = array();
 //destroy session
     session_destroy();
-    $message = "Uitgelogd";
-    echo "<script type='text/javascript'>alert('$message');</script>";
-    header("Location: ../../index.php");
-}
-else {
+    echo "<script>
+    alert('U bent nu uitgelogd!');
+    window.location.href='../../index.php';
+    </script>";
+} else {
     header("Location:login.php");
 }
 
