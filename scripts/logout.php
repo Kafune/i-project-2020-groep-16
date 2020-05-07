dbb0adc4e2686d0e1213bfab5a52c176effc4351
Coolcatss <j.vanaalst58@gmail.com>
2020-05-07 10:21:14 +0200

    uitlog script header

--- scripts/logout.php
+++ scripts/logout.php
@@ -8,7 +8,9 @@ if(isset($_SESSION["gebruikersnaam"])) {
     $_SESSION = array();
 //destroy session
     session_destroy();
-    echo "<h1>Uitgelogd</h1>";
+    $message = "Uitgelogd";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header("Location: ../../index.php");
 }
 else {
     header("Location:login.php");
