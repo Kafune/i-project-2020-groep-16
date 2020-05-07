3e881a22ac3ba9cd4205e963941f447e469a20dc
Coolcatss <j.vanaalst58@gmail.com>
2020-05-07 10:13:43 +0200

    Registratie formulieren en scripts

---
+++ scripts/registreren/gegevens_script.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+include_once('../../includes/db.php');
+
+echo $_SESSION['registratieStatus'];
+
+if ($_SESSION["registratieStatus"] == 3) {
+    echo "Doet het";
+    $email = $_SESSION['email'];
+    $gebruikersnaam = $_SESSION['gebruikersnaam'];
+    $wachtwoord = $_SESSION['wachtwoord'];
+    $voornaam = $_POST['voornaam'];
+    $achternaam = $_POST['achternaam'];
+    $adresregel1 = $_POST['adresregel1'];
+    $adresregel2 = $_POST['adresregel2'];
+    $postcode = $_POST['postcode'];
+    $plaatsnaam = $_POST['plaatsnaam'];
+    $land = $_POST['land'];
+    $geboortedag = $_POST['geboortedag'];
+    $vraag = $_POST['vraag'];
+    $antwoord = $_POST['antwoord'];
+
+    $sql = "INSERT INTO Gebruiker (gebruikersnaam, voornaam, achternaam, 
+            adresregel1, adresregel2, postcode, plaatsnaam, land, geboortedag, email, wachtwoord, 
+            vraag, antwoordtekst, isVerkoper)
+        VALUES (:gebruikersnaam, :voornaam, :achternaam, 
+            :adresregel1, :adresregel2, :postcode, :plaatsnaam, :land, :geboortedag, :email, :wachtwoord, 
+            :vraag, :antwoordtekst, 0)";
+
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':gebruikersnaam', $gebruikersnaam);
+    $stmt->bindParam(':wachtwoord', $wachtwoord);
+    $stmt->bindParam(':voornaam', $voornaam);
+    $stmt->bindParam(':achternaam', $achternaam);
+    $stmt->bindParam(':adresregel1', $adresregel1);
+    $stmt->bindParam(':adresregel2', $adresregel2);
+    $stmt->bindParam(':postcode', $postcode);
+    $stmt->bindParam(':plaatsnaam', $plaatsnaam);
+    $stmt->bindParam(':land', $land);
+    $stmt->bindParam(':geboortedag', $geboortedag);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':vraag', $vraag);
+    $stmt->bindParam(':antwoordtekst', $antwoord);
+
+    $stmt->execute();
+    echo "tot en met hier dan?";
+    $message = "Account aangemaakt!";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+
+    header('location: ../../index.php');
+
+} else {
+    echo "FOUT";
+    header('location:../../registratie/email.php');
+}
+?>
