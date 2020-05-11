1b21c979b3b23c75c0609c814c7db2e07c829e56
Coolcatss <j.vanaalst58@gmail.com>
2020-05-11 13:33:28 +0200

    verwijder gebruiker bug fix

--- scripts/verwijder-gebruiker.php
+++ scripts/verwijder-gebruiker.php
@@ -2,8 +2,8 @@
 session_start();
 include_once('../includes/db.php');
 
-if(isset($_SESSION['gebruikersnaam'])){
-    $gebruiker = $_SESSION['gebruikersnaam'];
+if(isset($_SESSION['gebruiker'])){
+    $gebruiker = $_SESSION['gebruiker'];
     $sql = "DELETE FROM gebruiker WHERE gebruikersnaam = ?";
     $q = $conn->prepare($sql);
     $res = $q->execute(array($gebruiker));
