f6285821bc260e26f245d2aed533703495c91bcb
Coolcatss <j.vanaalst58@gmail.com>
2020-05-11 13:10:43 +0200

    navbar mobile menu

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once ("root.php");
+include_once("root.php");
 ?>
 
 <!DOCTYPE html>
@@ -9,24 +9,27 @@ include_once ("root.php");
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?=$root?>/styles/css/mystyles.css">
+    <link rel="stylesheet" href="/styles/css/mystyles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
     <title>Eenmaal Andermaal</title>
 </head>
 <body>
 <nav class="navbar is-primary">
     <div class="navbar-brand">
-        <a class="navbar-item" href="index.php">
-            <img src= "<?=$root?>/sources/Logo.png" alt="Eenmaal Andermaal">
+        <a class="navbar-item" href="/index.php">
+            <img src="/sources/Logo.png" alt="Eenmaal Andermaal">
         </a>
-        <div class="navbar-burger burger" data-target="navMenuColorprimary-example">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+        <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
     </div>
 
 
-    <div class="navbar-menu">
+    <div class="navbar-menu" id="navMenu">
         <div class="navbar-start">
             <a class="navbar-item" href="/index.php">Home</a>
             <a class="navbar-item" href="#">Categoriën</a>
@@ -35,7 +38,7 @@ include_once ("root.php");
         </div>
         <div class="navbar-end">
             <?php
-            if($_SESSION['ingelogd']== false) {
+            if ($_SESSION['ingelogd'] == false) {
                 echo '<a class="button is-black" href="/registratie/email.php" style="margin-right: 2rem; margin-top: auto; margin-bottom: auto">Registreren</a>
                   <a class="button is-black" href="../login.php" style="margin-right: 2rem; margin-top: auto; margin-bottom: auto">Log In</a>';
             } else {
@@ -46,5 +49,31 @@ include_once ("root.php");
         </div>
     </div>
 </nav>
-<h1><?=$root?></h1>
 </body>
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                });
+            });
+        }
+
+    });
+</script>
