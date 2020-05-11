d2856167f638e5b815bed25ad3af5453911fe74e
Achraf <achrafouali@hotmail.com>
2020-05-11 13:29:30 +0200

    Verwijder gebruiker

--- scripts/verwijder-gebruiker.php
+++ scripts/verwijder-gebruiker.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['gebruikersnaam'])){
     $q = $conn->prepare($sql);
     $res = $q->execute(array($gebruiker));
     echo "<script>
-    alert('Account is verwijderd..');
+    alert('Account Has been deleted..');
     window.location.href='logout.php';
     </script>";
     $conn->close();
