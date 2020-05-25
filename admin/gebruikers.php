6f10e0d1a93c8b5faee90a29aa61958d56e999e9
Coolcatss <j.vanaalst58@gmail.com>
2020-05-25 17:06:35 +0200

    gebruikers filteren op admin pagina

--- admin/gebruikers.php
+++ admin/gebruikers.php
@@ -4,17 +4,27 @@ include_once('menu.php');
 include_once('../includes/db.php');
 
 if (isset($_GET['gebruikersnaam'])) {
-    $zoek = $_GET['gebruikersnaam'];
-    $stmt = $conn->prepare("SELECT * FROM Gebruiker WHERE gebruikersnaam LIKE '%" . $zoek . "%'");
-} else if (isset($_GET['voornaam'])) {
-    $zoek = $_GET['voornaam'];
-    $stmt = $conn->prepare("SELECT * FROM Gebruiker WHERE voornaam LIKE '%" . $zoek . "%'");
-} else if (isset($_GET['achternaam'])) {
-    $zoek = $_GET['achternaam'];
-    $stmt = $conn->prepare("SELECT * FROM Gebruiker WHERE achternaam LIKE '%" . $zoek . "%'");
-} else if (empty($_GET['gebruikersnaam'])) {
-    $stmt = $conn->prepare("SELECT * FROM Gebruiker ORDER BY gebruikersnaam");
+    $zoek = "SELECT * FROM Gebruiker
+         WHERE gebruikersnaam like '%".$_GET['gebruikersnaam']."%'
+         and voornaam like '%".$_GET['voornaam']."%'
+         and achternaam like '%".$_GET['achternaam']."%'";
+
+    if(isset($_GET['isVerkoper'])){
+        $zoek .= "and isVerkoper = 1";
+    }
+    if(isset($_GET['isAdmin'])){
+        $zoek .= "and isAdmin = 1";
+    }
+    if(isset($_GET['geblokkeerd'])){
+        $zoek .= "and geblokkeerd = 1";
+    }
+
+    $stmt = $conn->prepare($zoek);
+} else {
+    $zoek = "SELECT * FROM Gebruiker ORDER BY gebruikersnaam";
+    $stmt = $conn->prepare($zoek);
 }
+
 $stmt->execute();
 ?>
 
@@ -48,7 +58,7 @@ $stmt->execute();
                                 <td>" . $gebruikersnaam . "</td>
                                 <td>" . $naam . "</td>
                                 <td class=\"level-right\"><a class=\"button is-small is-primary\" 
-                                href='wijzigen/gebruiker_wijzigen.php?gebruikersnaam=".$gebruikersnaam."'>Wijzigen</a></td>
+                                href='wijzigen/gebruiker_wijzigen.php?gebruikersnaam=" . $gebruikersnaam . "'>Wijzigen</a></td>
                                 </tr>";
                             }
 
@@ -63,50 +73,51 @@ $stmt->execute();
             <div class="column">
                 <div class="card">
                     <header class="card-header">
-                        <p class="card-header-title">Zoeken op gebruikersnaam</p>
+                        <p class="card-header-title">Zoeken</p>
                     </header>
                     <form method="get" class="card-content">
                         <div class="content">
+                            <label class="label" for="gebruikersnaam">Gebruikersnaam</label>
                             <div class="control has-icons-left has-icons-right">
-                                <label class="label" for="gebruikersnaam"></label>
                                 <input class="input is-medium" type="text" id="gebruikersnaam" name="gebruikersnaam">
                                 <span class="icon is-medium is-left">
-                      <i class="fa fa-search"></i>
+                                <i class="fa fa-search"></i>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-            <div class="column">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">Zoeken op voornaam</p>
-                    </header>
-                    <form method="get" class="card-content">
                         <div class="content">
-                            <div class="control has-icons-left has-icons-right">
-                                <label class="label" for="voornaam"></label>
-                                <input class="input is-medium" type="text" id="voornaam" name="voornaam">
-                                <span class="icon is-medium is-left">
-                      <i class="fa fa-search"></i>
+                            <div class="content">
+                                <label class="label" for="voornaam">Voornaam</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input is-medium" type="text" id="voornaam" name="voornaam">
+                                    <span class="icon is-medium is-left">
+                                    <i class="fa fa-search"></i>
+                                </div>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-            <div class="column">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">Zoeken op achternaam</p>
-                    </header>
-                    <form method="get" class="card-content">
                         <div class="content">
+                            <label class="label" for="achternaam">Achternaam</label>
                             <div class="control has-icons-left has-icons-right">
-                                <label class="label" for="achternaam"></label>
                                 <input class="input is-medium" type="text" id="achternaam" name="achternaam">
                                 <span class="icon is-medium is-left">
-                      <i class="fa fa-search"></i>
+                                <i class="fa fa-search"></i>
                             </div>
+                        </div>
+                        <div class="content">
+                            <div class="control">
+                                <label class="label" for="isAdmin">Admin</label>
+                                <input class="checkbox" type="checkbox" id="isAdmin" value="1" name="isAdmin">
+                            </div>
+                            <div class="control">
+                                <label class="label" for="isVerkoper">Verkoper</label>
+                                <input class="checkbox" type="checkbox" id="isVerkoper" value="1" name="isVerkoper">
+                            </div>
+                            <div class="control">
+                                <label class="label" for="geblokkeerd">Geblokkeerd</label>
+                                <input class="checkbox" type="checkbox" id="geblokkeerd" value="1" name="geblokkeerd">
+                            </div>
+                        </div>
+                        <div class="control">
+                            <button class="button is-primary">Zoeken</button>
                         </div>
                     </form>
                 </div>
