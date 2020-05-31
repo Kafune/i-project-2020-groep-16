5c42164cf602d87e35a8d68ff7bf7b2defcbad87
Kafune <kachung97@hotmail.com>
2020-06-01 01:54:06 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
-//$gebruiker = "iproject16";
-//$wachtwoord = "zv1VeSWK";
-//
-//
-//try {
-//    $conn = new PDO('sqlsrv:server=mssql2.iproject.icasites.nl;database=iproject16', $gebruiker, $wachtwoord);
-//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//} catch (PDOException $e) {
-//    echo "Er is iets fout<br>{$e->getMessage()}";
-//}
+$gebruiker = "iproject16";
+$wachtwoord = "zv1VeSWK";
+
+
+try {
+    $conn = new PDO('sqlsrv:server=mssql2.iproject.icasites.nl;database=iproject16', $gebruiker, $wachtwoord);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Er is iets fout<br>{$e->getMessage()}";
+}
 
 //$gebruiker = "root";
 //$wachtwoord = "";
@@ -25,9 +25,9 @@ $gebruiker = "";
 $wachtwoord = "";
 
 //phpinfo();
-try {
-    $conn = new PDO("sqlsrv:server=(local);database=EenmaalAndermaal", $gebruiker, $wachtwoord);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Er is iets fout<br>{$e->getMessage()}";
-}
+//try {
+//    $conn = new PDO("sqlsrv:server=(local);database=EenmaalAndermaal", $gebruiker, $wachtwoord);
+//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//} catch (PDOException $e) {
+//    echo "Er is iets fout<br>{$e->getMessage()}";
+//}
