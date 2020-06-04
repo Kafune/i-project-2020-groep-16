76af359e5db09fbd542f8cf3a5219f10b3393027
Coolcatss <j.vanaalst58@gmail.com>
2020-06-04 10:13:02 +0200

    update voorwerpen voor het bod geplaatst is

--- scripts/bieden.php
+++ scripts/bieden.php
@@ -2,6 +2,12 @@
 include_once("../includes/db.php");
 session_start();
 
+$sql = "UPDATE Voorwerp
+SET veilinggesloten = (case when veilingeinde > CURRENT_TIMESTAMP THEN 0 ELSE 1 END)";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
 if (isset($_SESSION['gebruiker'])) {
     $gebruikersnaam = $_SESSION['gebruiker'];
 }
@@ -22,7 +28,7 @@ if (isset($_POST['bied'])) {
     $laatstebieder = $results['gebruiker'];
 
     /*Haal de startprijs op uit de de database*/
-    $sql_startprijs = "SELECT veilingGesloten, startprijs FROM Voorwerp WHERE voorwerpnummer = :voorwerpnummer";
+    $sql_startprijs = "SELECT veilingGesloten, startprijs, verkoper FROM Voorwerp WHERE voorwerpnummer = :voorwerpnummer";
     $stmt = $conn->prepare($sql_startprijs);
     $stmt->bindParam(':voorwerpnummer', $voorwerpnummer);
     $stmt->execute();
@@ -30,35 +36,41 @@ if (isset($_POST['bied'])) {
     $results = $stmt->fetch(PDO::FETCH_ASSOC);
     $startprijs = $results['startprijs'];
     $veilingGesloten = $results['veilingGesloten'];
+    $verkoper = $results['verkoper'];
 
-    if ($veilingGesloten == 0) {
-        if ($bod >= $startprijs) {
-            if ($bod > $hoogstebod) {
-                if ($gebruikersnaam != $laatstebieder) {
-                    $sql_insertbod = "INSERT INTO Bod(Voorwerp, bodbedrag, gebruiker, bodtijdstip) VALUES (:voorwerp, :bod, :gebruiker, :bodtijdstip)";
-                    $stmt = $conn->prepare($sql_insertbod);
-                    $stmt->bindParam(':voorwerp', $voorwerpnummer);
-                    $stmt->bindParam(':bod', $bod);
-                    $stmt->bindParam(':gebruiker', $gebruikersnaam);
-                    $stmt->bindParam(':bodtijdstip', $tijdstip);
+    if ($gebruikersnaam != $verkoper) {
+        if ($veilingGesloten == 0) {
+            if ($bod >= $startprijs) {
+                if ($bod > $hoogstebod) {
+                    if ($gebruikersnaam != $laatstebieder) {
+                        $sql_insertbod = "INSERT INTO Bod(Voorwerp, bodbedrag, gebruiker, bodtijdstip) VALUES (:voorwerp, :bod, :gebruiker, :bodtijdstip)";
+                        $stmt = $conn->prepare($sql_insertbod);
+                        $stmt->bindParam(':voorwerp', $voorwerpnummer);
+                        $stmt->bindParam(':bod', $bod);
+                        $stmt->bindParam(':gebruiker', $gebruikersnaam);
+                        $stmt->bindParam(':bodtijdstip', $tijdstip);
 
-                    $stmt->execute();
-                    header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
+                        $stmt->execute();
+                        header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
+                    } else {
+                        $_SESSION['error'] = 'zelfdeGebruiker';
+                        header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
+                    }
+
                 } else {
-                    $_SESSION['error'] = 'zelfdeGebruiker';
+                    $_SESSION['error'] = 'bodTeLaag';
                     header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
                 }
-
             } else {
-                $_SESSION['error'] = 'bodTeLaag';
+                $_SESSION['error'] = 'bodLagerDanStartprijs';
                 header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
             }
         } else {
-            $_SESSION['error'] = 'bodLagerDanStartprijs';
+            $_SESSION['error'] = 'veilingGesloten';
             header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
         }
     } else {
-        $_SESSION['error'] = 'veilingGesloten';
+        $_SESSION['error'] = 'biedenVerkoper';
         header('Location: ../voorwerp.php?voorwerpnummer=' . $voorwerpnummer . '');
     }
 }
