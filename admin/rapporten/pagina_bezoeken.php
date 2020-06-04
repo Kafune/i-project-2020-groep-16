afea878f5e2d41eb944cd98c845f3d7b62b32cc8
Coolcatss <j.vanaalst58@gmail.com>
2020-06-04 10:09:25 +0200

    Rapporten gemaakt

---
+++ admin/rapporten/pagina_bezoeken.php
@@ -0,0 +1,15 @@
+<?php
+include_once('../../includes/header.php');
+include_once('../menu.php');
+?>
+<div class="column is-10" style="padding-top: 3rem">
+    <section class="hero is-primary is-small">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">Meest bezochte pagina's</h1>
+            </div>
+        </div>
+    </section>
+    <iframe width="950.5" height="553.5" seamless frameborder="0" scrolling="no"
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSRaJ-OhdUqGdzLjPGYv-JxGKHqmyzauIiNDa3ytdU_7pgBfQZ5KtK5RFkrUDTMIl3yLeJcxEBCUrAk/pubchart?oid=1201696033&amp;format=interactive"></iframe>
+</div>
