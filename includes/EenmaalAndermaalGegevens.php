ce7f4c877fb511246d1f63f8bd40e912b35579db
Coolcatss <j.vanaalst58@gmail.com>
2020-06-10 14:47:08 +0200

    Gegevens eenmaal andermaal en db netjes opgemaakt

---
+++ includes/EenmaalAndermaalGegevens.php
@@ -0,0 +1,7 @@
+<?php
+
+// Voer hieronder uw email adres in voor het ontvangen van contact formulieren.
+$emailContact = "";
+
+// Voer hieronder uw email in voor het ontvangen van verificatie codes voor verkopers.
+$emailVerificatie = "";
